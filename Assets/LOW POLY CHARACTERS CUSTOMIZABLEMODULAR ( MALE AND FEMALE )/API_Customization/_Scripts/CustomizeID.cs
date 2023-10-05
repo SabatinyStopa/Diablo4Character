7fc459031b2a3796018fde3276eb2406aa8a8c30
sabatiny <sabatinystopa@gmail.com>
2023-10-05 19:11:39 -0300

    Initial commit

---
+++ Assets/LOW POLY CHARACTERS CUSTOMIZABLEMODULAR ( MALE AND FEMALE )/API_Customization/_Scripts/CustomizeID.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizeID : MonoBehaviour
+{
+    public int ID;
+    [Header ("Type of the object")]
+    public bool Hair;
+    public bool Hat, Acessories1, Acessories2, Acessories3, Gloves, Shirts, Bottoms, Shoes, SkinColor;
+    public CustomizeScript myreference;
+
+    private void Start()
+    {
+        if (Hair)
+        {
+            ID = PlayerPrefs.GetInt("HairID");
+        }
+        if (Hat)
+        {
+            ID = PlayerPrefs.GetInt("HatID");
+        }
+        if (Acessories1)
+        {
+            ID = PlayerPrefs.GetInt("Acessories1ID");
+        }
+        if (Acessories2)
+        {
+            ID = PlayerPrefs.GetInt("Acessories2ID");
+        }
+        if (Acessories3)
+        {
+            ID = PlayerPrefs.GetInt("Acessories3ID");
+        }
+        if (Gloves)
+        {
+            ID = PlayerPrefs.GetInt("GlovesID");
+        }
+        if (Shirts)
+        {
+            ID = PlayerPrefs.GetInt("ShirtsID");
+        }
+        if (Bottoms) 
+        { 
+            ID = PlayerPrefs.GetInt("BottomsID");
+        }
+        if (Shoes)
+        {
+            ID = PlayerPrefs.GetInt("ShoesID");
+        }
+        if (SkinColor)
+        {
+            ID = PlayerPrefs.GetInt("SkinID");
+        }
+    }
+    public void Update()
+    {
+        if (Hair)
+        {
+            PlayerPrefs.SetInt("HairID", ID);
+        }
+        if (Hat)
+        {
+            PlayerPrefs.SetInt("HatID", ID);
+        }
+        if (Acessories1)
+        {
+            PlayerPrefs.SetInt("Acessories1ID", ID);
+        }
+        if (Acessories2)
+        {
+            PlayerPrefs.SetInt("Acessories2ID", ID);
+        }
+        if (Acessories3)
+        {
+            PlayerPrefs.SetInt("Acessories3ID", ID);
+        }
+        if (Gloves)
+        {
+            PlayerPrefs.SetInt("GlovesID", ID);
+        }
+        if (Shirts)
+        {
+            PlayerPrefs.SetInt("ShirtsID", ID);
+        }
+        if (Bottoms)
+        {
+            PlayerPrefs.SetInt("BottomsID", ID);
+        }
+        if (Shoes)
+        {
+            PlayerPrefs.SetInt("ShoesID", ID);
+        }
+        if (SkinColor)
+        {
+            PlayerPrefs.SetInt("SkinID", ID);
+        }
+    }
+    //THIS PART CONTROLS THE BUTTONS OF CHANGING CLOTHES.
+    public void _Next()
+    {
+        if (Hair && myreference.IDHair < myreference.Hairs.Length)
+        {
+            ID += 1;
+        }
+        if (Hat && myreference.IDHat < myreference.Hats.Length)
+        {
+            ID += 1;
+        }
+        if (Acessories1 && myreference.IDAcessories < myreference.Acessories1.Length)
+        {
+            ID += 1;
+        }
+        if (Acessories2 && myreference.IDAcessories2 < myreference.Acessories2.Length)
+        {
+            ID += 1;
+        }
+        if (Acessories3 && myreference.IDAcessories3 < myreference.Acessories3.Length)
+        {
+            ID += 1;
+        }
+        if (Gloves && myreference.IDGloves < myreference.Gloves.Length)
+        {
+            ID += 1;
+        }
+        if (Shirts && myreference.IDShirts < myreference.Shirts.Length)
+        {
+            ID += 1;
+        }
+        if (Bottoms && myreference.IDBottoms < myreference.Bottoms.Length)
+        {
+            ID += 1;
+        }
+        if (Shoes && myreference.IDShoes < myreference.Shoes.Length)
+        {
+            ID += 1;
+
+        }
+        if (SkinColor && myreference.IDSkin < myreference.SkinMaterials.Length)
+        {
+            ID += 1;
+
+        }
+    }
+    public void _Previous()
+    {
+        if (Hair && myreference.IDHair >0)
+        {
+            ID -= 1;
+        }
+        if (Hat && myreference.IDHat > 0)
+        {
+            ID -= 1;
+        }
+        if (Acessories1 && myreference.IDAcessories > 0)
+        {
+            ID -= 1;
+        }
+        if (Acessories2 && myreference.IDAcessories2 > 0)
+        {
+            ID -= 1;
+        }
+        if (Acessories3 && myreference.IDAcessories3 > 0)
+        {
+            ID -= 1;
+        }
+        if (Gloves && myreference.IDGloves > 0)
+        {
+            ID -= 1;
+        }
+        if (Shirts && myreference.IDShirts > 0)
+        {
+            ID -= 1;
+        }
+        if (Bottoms && myreference.IDBottoms > 0)
+        {
+            ID -= 1;
+        }
+        if (Shoes && myreference.IDShoes > 0)
+        {
+            ID -= 1;
+        }
+        if (SkinColor && myreference.IDSkin > 0)
+        {
+            ID -= 1;
+        }
+    }
+}
---
+++ Assets/LOW POLY CHARACTERS CUSTOMIZABLEMODULAR ( MALE AND FEMALE )/API_Customization/_Scripts/CustomizeScript.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomizeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [HideInInspector]
+    public int IDHair, IDHat, IDAcessories, IDAcessories2, IDAcessories3, IDGloves, IDShirts, IDBottoms, IDShoes, IDSkin;
+    [Header ("Meshs of all Objects in Scene")]
+    public GameObject[] Hairs;
+    public GameObject[] Hats, Acessories1, Acessories2, Acessories3, Gloves, Shirts, Bottoms, Shoes;
+    [Header ("This is the UI objects with the CustomizeID script")]
+    public GameObject HairUI;
+    public GameObject HatUI, Acessories1UI, Acessories2UI, Acessories3UI, GlovesUI, ShirtsUI, BottomsUI, ShoesUI, SkinUI;
+
+    public GameObject BodyObject;
+    public Material[] SkinMaterials;
+    void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        //Load scenegame when press "e".
+        if (Input.GetKeyDown("e"))
+        {
+            SceneManager.LoadScene("GameSceneExample");
+        }
+        //------------------------------------------
+        CheckIDS();
+        ChangeHair();
+        ChangeHat();
+        ChangeAcessories();
+        ChangeAcessories2();
+        ChangeAcessories3();
+        ChangeGloves();
+        ChangeShirts();
+        ChangeBottoms();
+        ChangeShoes();
+        ChangeSkin();
+    }
+
+    void ChangeHair()
+    {
+        for (int i = 0; i < Hairs.Length; i++)
+        {
+            if (!Hairs[i].activeInHierarchy)
+            {
+                Hairs[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < Hairs.Length; i++)
+        {
+            if (Hairs[i].activeInHierarchy && i != IDHair)
+            {
+                Hairs[i].SetActive(false);
+            }
+        }
+    }
+    void ChangeHat()
+    {
+        for (int i = 0; i < Hats.Length; i++)
+        {
+            if (!Hats[i].activeInHierarchy)
+            {
+                Hats[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < Hats.Length; i++)
+        {
+            if (Hats[i].activeInHierarchy && i != IDHat)
+            {
+                Hats[i].SetActive(false);
+            }
+        }
+    }
+    void ChangeAcessories()
+    {
+        for (int i = 0; i < Acessories1.Length; i++)
+        {
+            if (!Acessories1[i].activeInHierarchy)
+            {
+                Acessories1[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < Acessories1.Length; i++)
+        {
+            if (Acessories1[i].activeInHierarchy && i != IDAcessories)
+            {
+                Acessories1[i].SetActive(false);
+            }
+        }
+    }
+    void ChangeAcessories2()
+    {
+        for (int i = 0; i < Acessories2.Length; i++)
+        {
+            if (!Acessories2[i].activeInHierarchy)
+            {
+                Acessories2[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < Acessories2.Length; i++)
+        {
+            if (Acessories2[i].activeInHierarchy && i != IDAcessories2)
+            {
+                Acessories2[i].SetActive(false);
+            }
+        }
+    }
+    void ChangeAcessories3()
+    {
+        for (int i = 0; i < Acessories3.Length; i++)
+        {
+            if (!Acessories3[i].activeInHierarchy)
+            {
+                Acessories3[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < Acessories3.Length; i++)
+        {
+            if (Acessories3[i].activeInHierarchy && i != IDAcessories3)
+            {
+                Acessories3[i].SetActive(false);
+            }
+        }
+    }
+    void ChangeGloves()
+    {
+        for (int i = 0; i < Gloves.Length; i++)
+        {
+            if (!Gloves[i].activeInHierarchy)
+            {
+                Gloves[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < Gloves.Length; i++)
+        {
+            if (Gloves[i].activeInHierarchy && i != IDGloves)
+            {
+                Gloves[i].SetActive(false);
+            }
+        }
+    }
+    void ChangeShirts()
+    {
+        for (int i = 0; i < Shirts.Length; i++)
+        {
+            if (!Shirts[i].activeInHierarchy)
+            {
+                Shirts[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < Shirts.Length; i++)
+        {
+            if (Shirts[i].activeInHierarchy && i != IDShirts)
+            {
+                Shirts[i].SetActive(false);
+            }
+        }
+    }
+    void ChangeBottoms()
+    {
+        for (int i = 0; i < Bottoms.Length; i++)
+        {
+            if (!Bottoms[i].activeInHierarchy)
+            {
+                Bottoms[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < Bottoms.Length; i++)
+        {
+            if (Bottoms[i].activeInHierarchy && i != IDBottoms)
+            {
+                Bottoms[i].SetActive(false);
+            }
+        }
+    }
+    void ChangeShoes()
+    {
+        for (int i = 0; i < Shoes.Length; i++)
+        {
+            if (!Shoes[i].activeInHierarchy)
+            {
+                Shoes[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < Shoes.Length; i++)
+        {
+            if (Shoes[i].activeInHierarchy && i != IDShoes)
+            {
+                Shoes[i].SetActive(false);
+            }
+        }
+    }
+    public void ChangeSkin()
+    {
+        for (int i = 0; i < IDSkin; i++)
+        {
+            BodyObject.GetComponent<SkinnedMeshRenderer>().material = SkinMaterials[i];
+        }
+    }
+    void CheckIDS()
+    {
+        IDHair = HairUI.GetComponent<CustomizeID>().ID;
+        IDHat = HatUI.GetComponent<CustomizeID>().ID;
+        IDAcessories = Acessories1UI.GetComponent<CustomizeID>().ID;
+        IDAcessories2 = Acessories2UI.GetComponent<CustomizeID>().ID;
+        IDAcessories3 = Acessories3UI.GetComponent<CustomizeID>().ID;
+        IDGloves = GlovesUI.GetComponent<CustomizeID>().ID;
+        IDShirts = ShirtsUI.GetComponent<CustomizeID>().ID;
+        IDBottoms = BottomsUI.GetComponent<CustomizeID>().ID;
+        IDShoes = ShoesUI.GetComponent<CustomizeID>().ID;
+        IDSkin = SkinUI.GetComponent<CustomizeID>().ID;
+    }
+}
